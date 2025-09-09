b8d0b6fa01fe4000b85b043afbaf082949ba9cb8
Hannah Normeshie <elikemnormeshie2@gmail.com>
2025-09-09 15:46:48 +0000

    updatethree

--- pages/home.py
+++ pages/home.py
@@ -3,3 +3,7 @@ from nicegui import ui
 
 def show_home_page():
     ui.label("Welcome to the Advertisement Manager Home Page")
+    with ui.element("div").style("background-image: url(./assets/hero.jpg)").classes(
+        "h-screen w-screen flex flex-col justify-center items-center bg-cover bg-center"
+    ):
+        ui.button("Create ads")
